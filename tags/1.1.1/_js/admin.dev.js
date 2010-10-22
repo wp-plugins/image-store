e7bb12b82677ef7f074f5af885e124bc0600f960
Hax <Hax@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-22 19:18:54 +0000

    Release 1.1.1 tag
    git-svn-id: https://plugins.svn.wordpress.org/image-store@303141 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.1.1/_js/admin.dev.js
@@ -0,0 +1,329 @@
+jQuery(document).ready(function($){
+	
+	postboxes.add_postbox_toggles(pagenow);
+	
+	/******** TABS **********/
+
+	//tabs default state
+	$('.imstore .ims-box').hide();
+	$('.imstore .ims-box').eq(0).show();
+	$('.ims-tabs li').eq(0).addClass('current');
+			
+	//tabs actions
+	$('.ims-tabs li').click(function(){
+		$('#message').remove();
+		$('.imstore .ims-box').hide();
+		$('.ims-tabs li').removeClass('current');
+		$('.imstore .ims-box').eq($('.ims-tabs li').index( $(this) )).show();
+		$(this).addClass('current');
+		return false;
+	});
+	
+	//tabs select
+	if( hash = window.location.hash ){
+		$('.imstore .ims-box').hide();
+		$('.ims-tabs li').removeClass('current');
+		index = $('.ims-tabs li a').index($('a[href|=' + hash + ']'));
+		$('.ims-tabs li').eq(index).addClass('current');
+		$( hash ).show();
+		$('html, body').animate({scrollTop: '0px'});
+	};
+	
+	//permissions: user dropdown
+	$('#ims_user').change(function(){
+		if( $(this).val() > 0 ){
+			window.location.hash = 'caps_settings';
+			window.location.search = 'page=ims-settings&userid='+ $(this).val() ;
+			window.location.href = window.location ;
+		}
+	});
+	
+	//open thickbox
+	$('#addwatermarkurl').live('click', function(){
+		win = window.dialogArguments || opener || parent || top;
+		tb_show('Attach File', 'media-upload.php?imstore=1&TB_iframe=true'); 				 
+	});
+	
+	//add watermark url
+	window.add_watermark_url = function( image ) {
+		jQuery('#watermarkurl').val( image );
+		tb_remove();
+	};
+	
+	//add image size
+	$('#addimagesize').click( function( ){
+		var count = $('.image-size').length;
+		var clas = ( count % 2 ) ? '' : ' alternate';
+		var row = '<tr class="t image-size'+clas+'"><td scope="row">';
+		row += '<input type="checkbox" name="imgid_'+count+'" class="inputmd" /></td>';
+  row += '<td><input type="text" name="imagesize_'+count+'[name]" class="inputmd" /></td>';
+		row += '<td><label><input type="text" name="imagesize_'+count+'[w]" class="inputsm" /></label></td>';
+		row += '<td><label><input type="text" name="imagesize_'+count+'[h]" class="inputsm" /></label></td>';
+		row += '<td><label><input type="text" name="imagesize_'+count+'[q]" class="inputsm" />(%)</label></td>';
+		row += '<td>'+ imslocal.pixels +'</td>';
+		row += '<td>&nbsp;</td></tr>';
+		$('.ims-image-sizes').before( row );	
+		return false;
+	});
+	
+	/******** DRAG/DROP/SORT **********/
+	
+	$(".price-list tbody").sortable({
+		//revert: true,
+		cursor: 'move',
+		handle: '.move',
+		placeholder: 'widget-placeholder',
+		stop: function(event, ui){
+			$("tr.filler").hide();
+			ui.item.attr( 'class', 'alternate size' ).removeAttr('style'); 
+		}
+	});
+	
+	$("#price-list .sizes-list .size, #price-list .package-list .size").draggable({
+		helper: 'clone',
+		revert: 'invalid',
+		handle: '.move',
+		connectToSortable: '.price-list tbody'
+	});
+	
+	$("#packages .package-list tbody").sortable({
+		cursor: 'move',
+		handle: '.move',
+		placeholder: 'widget-placeholder',
+		stop: function(event, ui){
+			$("tr.filler").hide();
+			ui.item.find('.price').remove();
+			ui.item.attr('class', 'alternate size').removeAttr('style'); 
+		}
+	});
+	
+	$("table.sort-images tbody").sortable({
+		axis: 'y',
+		cursor: 'move',
+		helper: 'clone',
+		placeholder: 'widget-placeholder',
+		update : function () { 
+			$(this).find('tr').each(function(i) {
+				$(this).find('.column-order input').val(i+1);
+				if( (i%2) != 0 ) $(this).removeClass('alternate').addClass('alternate');
+				else $(this).removeClass('alternate');
+			});
+		} 
+	});
+	
+	$("#packages .sizes-list .size").draggable({
+		helper: 'clone',
+		revert: 'invalid',
+		handle: '.move',
+		connectToSortable: '.package-list tbody'
+	});
+
+	$("tr.size").disableSelection();
+	$('td.x').live( 'click', function(){
+			if( $(this).parent().parent().find('tr.size').length <= 1 ) 
+				$('tr.filler').show();
+			$(this).parent().remove();
+		}
+	);
+	
+	/******** WIDGETS **********/
+	
+	//default state
+	$('.show-free').hide();
+	$('tbody.content').hide();
+	$('tbody.content').hide();
+	$('tfoot.content').hide();
+	$('.show-download').hide();
+	
+	//show/hide widget list content
+	$('.itemtop a').toggle( 
+		function( ){ 
+			$(this).html('[-]'); 
+			index = $('.itemtop a').index($(this));
+			$('tbody.content').eq(index).show( );
+			$('tfoot.content').eq(index).show( );
+			if( $('tbody.content').eq(index).find('tr.size').length <= 0 )
+				$('tr.filler').eq(index).show( );
+			else $('tr.filler').eq(index).hide( );
+			$(".price-list tbody").sortable( "refresh" );
+			$(".package-list tbody").sortable( "refresh" );
+		},	
+		function( ){ 
+			$(this).html('[+]') ;
+			index = $('.itemtop a').index($(this));
+			$('tbody.content').eq(index).hide();
+			$('tfoot.content').eq(index).hide();
+			$('tr.filler').eq(index).hide( );
+			$(".price-list tbody").sortable( "refresh" );
+			$(".package-list tbody").sortable( "refresh" );
+		}
+	);
+	
+	//trash pricelist
+	$("#price-list .trash").click(function(){
+		del = confirm( imslocal.deletelist );
+		if( del ){
+			id = $(this).parent().find('.listid').val();
+			$.get( imslocal.imsajax, { 	
+				action: 'deletelist',
+				listid: id,
+				_wpnonce: imslocal.nonceajax
+			}, function(){$('#ims-list-'+id).remove() });
+			
+		}
+		return false;
+	});
+	
+	
+	//trash pricelist
+	$("#packages .trash").click(function(){
+		del = confirm( imslocal.deletepackage );
+		if( del ){
+			id = $(this).parent().find('.packageid').val();
+			$.get( imslocal.imsajax, { 	
+				action: 'deletepackage',
+				packageid: id,
+				_wpnonce: imslocal.nonceajax
+			}, function(){$('#package-list-'+id).remove()});
+			
+		}
+		return false;
+	});
+	
+	//trash item
+	$("span.delete a").click(function(){
+		del = confirm( imslocal.deleteentry );
+		if( del ){ 
+			return true;
+		}
+		return false;
+	});
+	
+	//promotions 
+/*	$("#promo_type").change(function(){
+		if($(this).val() == 3 ){
+			$('.show-free').show();
+			$('.hide-free').hide();
+		}else{
+			$('.show-free').hide();
+			$('.hide-free').show();
+			$(".show-download").hide();
+		}
+	});
+	*/
+	
+	//promotions 
+	$("#promo_type").change(function(){
+		if( $(this).val() == 3 ){
+			$('input[name="discount"]').attr({disabled:"disabled"});
+		}else{
+			$('input[name="discount"]').removeAttr('disabled');
+		}
+	});
+	
+	//add add size
+	$(".add-image-size").click(function(){
+		counter = $(this).parents('.postbox').find(".copyrow .name").val();
+		row = $(this).parents('.postbox').find(".copyrow").clone().removeClass('copyrow');
+		row.find('.name').attr( 'name','sizes['+counter+'][name]').removeAttr('value');
+		row.find('.price').attr( 'name','sizes['+counter+'][price]');
+		row.find('.unit').attr( 'name','sizes['+counter+'][unit]');
+		$(this).parents('.postbox').find(".addrow").before(row);
+		$(this).parents('.postbox').find(".copyrow .name").val(counter++);
+		return false;
+	});
+
+	
+	/******** IMAGE UPLOAD **********/
+	
+	var exists = 0;
+	var uploaded = 0;
+	
+	//remove message
+	function ims_file_selected( ){
+		$('#message').remove( );
+	};
+	
+	if( $('input[name=disableflash]').length > 0 ){
+		
+		//change folder path when event is selected
+		$('#upload-images input[type=radio]').click(function( ){
+			$.get( imslocal.imsajax, { 
+				action		: 'swuploadfolder',
+				galleryid	: $(this).val( ),
+				_wpnonce	: imslocal.nonceajax
+				}, function( data ){
+				 $( '#imagefiles' ).uploadifySettings( 'folder', data );
+			});
+		});
+
+		$("#imagefiles").uploadify({
+			'buttonText'	 : imslocal.flastxt,
+			'multi' 		 : true,
+			'uploader' 		 : imslocal.imsurl + '_swf/uploadify.swf',
+			'script' 		 : imslocal.imsurl + 'admin/swfupload.php',
+			'scriptData' 	 : {'action' : 'swupload'},
+			'cancelImg' 	 : imslocal.imsurl + '_img/xit.gif',
+			//'wmode'		 : 'transparent',
+			'height'		 : '26',
+			'width'			 : '118',
+			'fileExt'		 : '*.jpg;*.jpeg;*.gif;*.png',
+			'fileDesc'		 : 'Image files',
+			'onSelect'		 : ims_file_selected,
+			'onComplete'	 : ims_file_uploaded,
+			'onAllComplete'	 : ims_upload_complete,
+			'buttonTextColor' : '#333333'
+		});
+		
+	}
+		
+	//add image: event upload files
+	$('.upload-images').click(function(){
+		exists = 0;
+		uploaded = 0;
+		 if( $('#upload-images input[type=radio]:checked').length <= 0 ){
+			 alert( imslocal.selectgal );
+			 return false; 
+		 }else{ $('#imagefiles').uploadifyUpload( ); }
+		 return false; 
+	 });
+
+	// run every time a file is uploaded
+	function ims_file_uploaded( event, ID, fileObj, response, data ){
+		if(response == 'x'){
+			exists++;
+		}else{
+			$.get( imslocal.imsajax, { 
+				action		: 'flashimagedata',
+				galleryid	: $('#upload-images input[type=radio]:checked').val( ),
+				imagename 	: fileObj.name,
+				filepath	: response,
+				_wpnonce	: imslocal.nonceajax
+			}/*, function( data ){ alert( data )}*/);
+			uploaded ++; 
+		}
+	};
+	
+	//redirect after files uploaded
+	function ims_upload_complete( event, data ){
+		setTimeout(function( ){
+			$( '<div class="updated fade" id="message"><p>' + 
+				uploaded + imslocal.uploaded + 
+				exists	+ imslocal.exists + 
+				'</p></div>').insertBefore('.add-menu-item-tabs');
+		}, 2000 );
+	};
+	
+	//show uploading icon
+	$('#zipupload, #importfolder, #rebuildimgs').click(function(){
+		$(this).parents('td').find('div.loading').show( );	
+	});
+	
+	/******** DATE PICKERS **********/
+	$("#date").datepicker({ altField: '#post_date', altFormat: 'yy-m-d', dateFormat: imslocal.dateformat });
+	$("#expire").datepicker({ altField: '#ims_expire', altFormat: 'yy-m-d', dateFormat: imslocal.dateformat });
+	
+	$("#starts").datepicker({ altField: '#start_date', altFormat: 'yy-m-d', dateFormat: imslocal.dateformat });
+	$("#expires").datepicker({ altField: '#expiration_date', altFormat: 'yy-m-d', dateFormat: imslocal.dateformat });
+	
+});
---
+++ tags/1.1.1/includes/shoppingcart.php
@@ -0,0 +1,176 @@
+<?php 
+
+/**
+ * Shopping cart page
+ * 
+ * @package Image Store
+ * @author Hafid Trujillo
+ * @copyright 20010-2011
+ * @since 0.5.0 
+*/
+
+// Stop direct access of the file
+if( preg_match( '#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'] ) ) 
+	die( );
+
+
+$sym 	= $this->opts['symbol']; 
+$loc 	= $this->opts['clocal'];
+$format = array( '', "$sym%s", "$sym %s", "%s$sym", "%s $sym"); 
+$colors_options = array(
+	'ims_sepia' => __( 'Sepia + ', ImStore::domain ),	
+	'color' 	=> __( 'Full Color', ImStore::domain ),	
+	'ims_bw' 	=> __( 'B &amp; W + ', ImStore::domain )
+);
+
+?>
+<div id="ims-mainbox" class="shopping-cart">
+	
+	<div class="ims-nav-box"><?php $this->store_nav( )?></div>
+	
+	<div class="ims-labels">
+		<span class="title"><?php echo $this->gallery->post_title?></span>
+		<?php if( $this->gallery->post_expire != '0000-00-00 00:00:00' ){ ?>
+		<span class="divider"> | </span>
+		<span class="expires"><?php 
+			echo __( "Expires: ", ImStore::domain ) . date_i18n( get_option( 'date_format' ), strtotime( $this->gallery->post_expire ))
+		?></span>
+		<?php }?>
+	</div>
+	
+	
+	<div class="ims-message<?php echo $css?>">
+		<?php if( $this->error ) echo $this->error?>
+		<?php if( $this->message ) echo $this->message?>
+	</div>
+	
+	
+	<div class="ims-innerbox">
+	
+	<?php if( empty( $this->cart['images'] ) ) {?>
+	
+		<div class="ims-message error"><?php _e( 'Your shopping car is empty!!', ImStore::domain )?></div>
+	
+	<?php }else{?>
+			<form action="" method="post">
+			<table class="ims-table">
+				<thead>
+					<tr>
+						<th scope="col" class="preview">&nbsp;</th>
+						<th scope="col" colspan="6" class="subrows">
+							<span class="quantity"><?php _e( 'Quantity', ImStore::domain )?></span>
+							<span class="size"><?php _e( 'Size', ImStore::domain )?></span>
+							<span class="color"><?php _e( 'Color', ImStore::domain )?></span>
+							<span class="price"><?php _e( 'Unit Price', ImStore::domain )?></span>
+							<span class="subtotal"><?php _e( 'Subtotal', ImStore::domain )?></span>
+							<span class="delete"><?php _e( 'Delete', ImStore::domain )?></span>
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php $i=1; foreach( $this->cart['images'] as $id => $sizes ){?>
+					<?php $image = get_post_meta( $id, '_wp_attachment_metadata', true )?>
+					<tr>
+						<td scope="row" class="preview"><img src="<?php echo $image['sizes']['mini']['url']?>" width="<?php echo $image['sizes']['mini']['width']?>" height="<?php echo $image['sizes']['mini']['height']?>" alt="<?php echo $image['sizes']['mini']['file']?>"/></td>
+						<td colspan="6" class="subrows">
+						<?php foreach( $sizes as $size => $colors ){?>
+							<?php foreach( $colors as $color => $item ){?>
+							<div class="clear-row">
+								<span class="quantity"><input type="text" name="ims-quantity<?php echo "[{$id}][{$size}][{$color}]"?>" value="<?php echo $item['quantity']?>" class="input" /></span>
+								<span class="size"><?php echo $size . ' ' . $item['unit']?></span>
+								<span class="color"><?php echo $colors_options[$color] . $item['color'] ?></span>
+								<span class="price"><?php printf( $format[$loc], number_format( $item['price'], 2 ) )?></span>
+								<span class="subtotal"><?php printf( $format[$loc], number_format( $item['subtotal'], 2 ) )?></span>
+								<span class="delete"><input name="ims-remove[]" type="checkbox" value="<?php echo "{$id}|{$size}|{$color}"?>" /></span>
+								<input type="hidden" name="on0_<?php echo $i?>" value="<?php echo $size ?>"/>
+								<input type="hidden" name="os0_<?php echo $i?>" value="<?php echo $color?>"/>
+								<input type="hidden" name="quantity_<?php echo $i?>" value="<?php echo $item['quantity']?>"/>
+								<input type="hidden" name="amount_<?php echo $i?>" value="<?php echo $item['price'] + $item['color'];?>"/>
+								<input type="hidden" name="item_name_<?php echo $i ?>" value="image-<?php echo sprintf( "%05d", $id );?>"/>
+							</div>
+							<?php $i++; }?>
+						<?php }?>
+						</td>
+					</tr>
+				<?php }?>
+				</tbody>
+				<tfoot>
+					<tr>
+						<td scope="row">&nbsp;</td>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+						<td><?php _e( 'Item subtotal',ImStore::domain )?></td>
+						<td class="total" colspan="2"><?php printf( $format[$loc], number_format( $this->cart['subtotal'], 2 ) )?></td>
+					</tr>
+					<tr>
+						<td scope="row"colspan="4">&nbsp;</td>
+						<td><?php _e( 'Promotional code',ImStore::domain )?></td>
+						<td class="total promo-code" colspan="2"><input name="promocode" type="text" value="<?php echo $this->cart['promo']['code']?>" /></td>
+					</tr>
+					<tr>
+						<td scope="row" colspan="4">&nbsp;</td>
+						<td><?php _e( 'Shipping', ImStore::domain )?></td>
+						<td colspan="2" class="shipping">
+							<?php $meta = get_post_meta( $this->pricelist_id, '_ims_list_opts', true );?>
+							<select name="shipping_1" id="shipping_1" class="shipping-opt">
+								<option value="<?php echo $meta['ims_ship_local']?>"<?php $this->selected( $meta['ims_ship_local'], $this->cart['shipping'] )?>><?php echo __( 'Local + ', ImStore::domain ) . sprintf( $format[$loc], $meta['ims_ship_local'] )?></option>
+								<option value="<?php echo $meta['ims_ship_inter']?>"<?php $this->selected( $meta['ims_ship_inter'], $this->cart['shipping'] )?>><?php echo __( 'International + ', ImStore::domain ) . sprintf( $format[$loc], $meta['ims_ship_inter'] )?></option>
+							</select>
+						</td>
+					</tr>
+					<?php if( $this->cart['discounted'] ){ ?>
+					<tr>
+						<td scope="row" colspan="4">&nbsp;</td>
+						<td><?php _e( 'Discount',ImStore::domain )?></td>
+						<td colspan="2" class="discount"><?php printf( '- ' .$format[$loc], number_format( $this->cart['promo']['discount'], 2 ) ) ?></td>
+					</tr>
+					<?php } ?>
+					<?php if( $this->cart['tax'] ){ ?>
+					<tr>
+						<td scope="row" colspan="4">&nbsp;</td>
+						<td><?php _e( 'Tax',ImStore::domain )?></td>
+						<td colspan="2" class="tax">
+							<?php printf( '+ ' . $format[$loc], number_format( $this->cart['tax'], 2 ) ) ?>
+							<input type="hidden" name="tax_cart" value="<?php echo number_format( $this->cart['tax'], 2 ) ?>"/>
+						</td>
+					</tr>
+					<?php } ?>
+					<tr>
+						<td scope="row" colspan="4">&nbsp;</td>
+						<td><?php _e( 'Total',ImStore::domain )?></td>
+						<td colspan="2" class="total"><?php printf( $format[$loc], number_format( $this->cart['total'], 2 ) ) ?></td>
+					</tr>
+					<tr>
+						<td scope="row" colspan="4">&nbsp;</td>
+						<td colspan="3">
+							<input name="applychanges" type="submit" value="<?php _e( 'Update Cart', ImStore::domain )?>" class="secondary" />
+							<input name="<?php echo ($this->opts['gateway'] == 'notification' ) ? 'enotification' : 'checkout' ?>" type="submit" value="<?php _e( 'Check out', ImStore::domain )?>" class="primary" />
+						</td>
+					</tr>
+				</tfoot>
+			</table>
+			
+			<div class="ims-terms-condtitions"><?php echo $this->opts['termsconds'] ?></div>
+			
+			<input type="hidden" name="ims-total" value="<?php echo $this->cart['total'] ?>" />
+
+			<input type="hidden" name="rm" value="2" />
+			<input type="hidden" name="upload" value="1"/>
+			<input type="hidden" name="cmd" value="_cart"/>
+			<input type="hidden" name="lc" value="<?php echo $this->opts['currency'] ?>" />
+			<input type="hidden" name="page_style" value="<?php bloginfo( 'name' )?>"/>
+			<input type="hidden" name="custom" value="<?php echo $this->cart_cookie ?>"/>
+			<input type="hidden" name="return" value="<?php echo $this->get_permalink( $this->gallery_id, 8 )?>"/>
+			<input type="hidden" name="notify_url" value="<?php echo $this->get_permalink( $this->gallery_id, 6 )?>"/>
+			<input type="hidden" name="cancel_return" value="<?php echo $this->get_permalink( $this->gallery_id, 7 )?>"/>
+			<input type="hidden" name="business" value="<?php echo $this->opts['paypalname']?>" />
+			<input type="hidden" name="currency_code" value="<?php echo $this->opts['currency']?>" />
+			<input type="hidden" name="_wpnonce" id="_wpnonce" value="<?php echo wp_create_nonce( "ims_submit_order" )?>" />					
+			<input type="hidden" name="cbt" value="<?php printf( __( 'Return to %s', ImStore::domain ), get_bloginfo( 'name' ) )?>" />
+			<input type="hidden" name="discount_amount_cart" value="<?php printf( __( 'Return to %s', ImStore::domain ), get_bloginfo( 'name' ) )?>" />
+		</form>
+	<?php }?>
+	</div>
+	
+</div>
